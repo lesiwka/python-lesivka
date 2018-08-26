fccad033dfcd243ed402e2d621c2f52f56f71e35
Mystic-Mirage <mm@m10e.net>
2018-08-26 19:35:42 +0300

    Fix apostrophes

--- lesivka/_encode/rule_3_1.py
+++ lesivka/_encode/rule_3_1.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 
 from ..utils import applier, replacer
 
-APOSTROPHES = "'`’’''"
+APOSTROPHES = "'`\u2018\u2019\u201b\u2032\u2035"
 
 apply = applier(replacer({c: '' for c in APOSTROPHES}))
