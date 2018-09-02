00b44750b460a196d99000a18f884b91ec9b388c
Mystic-Mirage <mm@m10e.net>
2018-09-02 18:38:13 +0300

    Remove debug print

--- lesivka/_encode/rule_3_1.py
+++ lesivka/_encode/rule_3_1.py
@@ -10,7 +10,6 @@ def remove_apostrophe():
     pattern = re.compile('(\w)([%s])(\w)' % APOSTROPHES, re.UNICODE)
 
     def remove(text):
-        print(pattern.split(text))
         return ''.join(t for t in pattern.split(text) if t not in APOSTROPHES)
 
     return remove
